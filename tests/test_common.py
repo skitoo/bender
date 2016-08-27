02a3a1b51d03043f14dd45ea47a5e7d837f187ea
Alexis Couronne <alexis.couronne@gandi.net>
2016-08-27 11:44:53 +0200

    Create 'common' role

---
+++ tests/test_common.py
@@ -0,0 +1,28 @@
+from testinfra.utils.ansible_runner import AnsibleRunner
+
+testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
+
+
+def test_apt_transport_https_is_installed(Package):
+    pkg = Package('apt-transport-https')
+    assert pkg.is_installed
+
+
+def test_tree_is_installed(Package):
+    pkg = Package('tree')
+    assert pkg.is_installed
+
+
+def test_pwgen_is_installed(Package):
+    pkg = Package('pwgen')
+    assert pkg.is_installed
+
+
+def test_git_is_installed(Package):
+    pkg = Package('git')
+    assert pkg.is_installed
+
+
+def test_htop_is_installed(Package):
+    pkg = Package('htop')
+    assert pkg.is_installed
