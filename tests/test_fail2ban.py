725f54fb8218ade5d700cb639dd34becbbec746c
Alexis Couronne <alexis.couronne@gandi.net>
2016-09-04 16:49:28 +0200

    Add some tests to check fail2ban tunning

--- tests/test_fail2ban.py
+++ tests/test_fail2ban.py
@@ -12,3 +12,23 @@ def test_fail2ban_running_and_enabled(Service):
     fail2ban = Service('fail2ban')
     assert fail2ban.is_running
     assert fail2ban.is_enabled
+
+
+def test_jail_local_file_exists(File):
+    assert File('/etc/fail2ban/jail.local').exists
+
+
+def test_nginx_badbots_file_exists(File):
+    assert File('/etc/fail2ban/filter.d/nginx-badbots.conf').exists
+
+
+def test_nginx_nohome_file_exists(File):
+    assert File('/etc/fail2ban/filter.d/nginx-nohome.conf').exists
+
+
+def test_nginx_noproxy_file_exists(File):
+    assert File('/etc/fail2ban/filter.d/nginx-noproxy.conf').exists
+
+
+def test_nginx_noscript_file_exists(File):
+    assert File('/etc/fail2ban/filter.d/nginx-noscript.conf').exists
