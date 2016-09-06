16a6a86c10482b45431280729e3e5d9dd6e967d8
Alexis Couronne <alexis.couronne@gandi.net>
2016-09-06 14:02:55 +0200

    Fix docker tests

--- tests/test_docker.py
+++ tests/test_docker.py
@@ -4,11 +4,11 @@ testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
 def test_docker_is_installed(Package):
-    pkg = Package('docker-service')
+    pkg = Package('docker-engine')
     assert pkg.is_installed
 
 
 def test_docker_running_and_enabled(Service):
-    docker = Service('docker-service')
+    docker = Service('docker')
     assert docker.is_running
     assert docker.is_enabled
